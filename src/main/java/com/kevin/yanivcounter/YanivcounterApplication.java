4f3c29fdc777ce8123b1bfb2a8bef6b0d5e01300
Kevin Huang <krythuang@gmail.com>
2023-10-29 20:32:13 +0100

    Test

---
+++ src/main/java/com/kevin/yanivcounter/YanivcounterApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.yanivcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanivcounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanivcounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/yanivcounter/YanivcounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.yanivcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanivcounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
